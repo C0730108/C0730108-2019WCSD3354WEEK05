81587258bb11c6e9f5dab13577ba93dc8a82df94
c0730108 <jashandeepkaur717@gmail.com>
2019-02-07 17:04:54 -0500

    4.50commit

--- C0730108/Program.cs
+++ C0730108/Program.cs
@@ -13,10 +13,12 @@ namespace C0730108
         var a = new TestQuestion2();
         //a.PlayingWithForLoops();  
 
-        var b = new Birthday_party();
+        var b = new birthday_party();
         b.setupPartyList();
-        b.printPartyList();
-      }
+       
+            Console.WriteLine(b.printPartyList());
+            Console.WriteLine(b.printPartyList_reverce());
+        }
    }
 
     class TestQuestion2
@@ -50,12 +52,11 @@ namespace C0730108
 
     class dog
         {
-            public dog(string name, string breed, dog next, dog prev)
+            public dog(string name, string breed)
             {
                 dog_name = name;
                 dog_breed = breed;
-                next_dog = next;
-                prev_dog = prev;
+                
             }
             public string dog_name;
             public string dog_breed;
@@ -67,48 +68,72 @@ namespace C0730108
             public dog peanut;
             public dog fifi;
             public dog clarance;
+            public dog giselle;
+            public dog lulu;
             public dog roy;
 
             public dog head;
             public dog tail;
             public dog temporary;
             public void setupPartyList()
-
-
-
-            
-
-
-
+  
             {
                 peanut = new dog("peanut", "Bichon");
                 fifi = new dog("fifi", "Poodle");
                 clarance = new dog("Clarance", "German Sheppard");
-                roy = new dog("Roy", "Beagle");
+             lulu = new dog("Lulu", "Shitzu");
+            giselle = new dog("Giselle", "Border Collie");
+            roy = new dog("Roy", "Beagle");
 
                 peanut.prev_dog = null;
                 peanut.next_dog = fifi;
                 fifi.prev_dog = peanut;
                 fifi.next_dog = clarance;
                 clarance.prev_dog = fifi;
-                fifi.next_dog = roy;
-                roy.prev_dog = clarance;
-                fifi.next_dog = null;
+                clarance.next_dog = roy;
+            giselle.prev_dog = clarance;
+            giselle.next_dog = lulu;
+            lulu.prev_dog = fifi;
+            lulu.next_dog = roy;
+            roy.prev_dog = lulu;
+                roy.next_dog = null;
                 head = peanut;
                 tail = roy;
 
             }
             public string printPartyList()
             {
-                string inviteList = "*____*";
+                string inviteList = "*---";
                 temporary = head;
                 while (temporary.next_dog != null) ;
             
             {
-            inviteList+= temporary.dog_name + "___";
-}
+                Console.WriteLine(temporary.next_dog.dog_name);
+            inviteList+= temporary.dog_name + "*---*";
+                temporary = temporary.next_dog;
+             }
+            inviteList += temporary.dog_name + "*---*";
             return inviteList;
-    }
+            }
+
+
+
+
+        public string printPartyList_reverce()
+        {
+            string inviteList = "*---";
+            temporary = tail;
+            while (temporary.prev_dog != null) ;
+
+            {
+                Console.WriteLine(temporary.prev_dog.dog_name);
+                inviteList += temporary.dog_name + "*---*";
+                temporary = temporary.prev_dog;
+            }
+            inviteList += temporary.dog_name + "*---*";
+            return inviteList;
+        }
+
     }
 }
 
