303427a8552c2f9afb74c91f60b216cfa5f9cba4
c0730108 <jashandeepkaur717@gmail.com>
2019-02-07 14:38:25 -0500

    2:05 PM COMMIT

--- C0730108/Program.cs
+++ C0730108/Program.cs
@@ -10,6 +10,34 @@ namespace C0730108
     {
         static void Main(string[] args)
         {
+            var a = new TestQuestion2();
+            a.PlayingWithForLoops();
         }
+        class TestQuestion2
+        {
+            public int myFavouriteVariables = 0;
+            public void PlayingWithForLoops()
+            {
+                // Write a for loop to Add 10 numbers
+                for(; MyMethod();)
+                {
+                    if(myFavouriteVariables>10)
+                    {
+                        Console.WriteLine("i am so out of here");
+                        break;
+
+                    }
+                    Console.WriteLine("oh no I have to go through this stupid loop again");
+
+                }
+            }
+            public bool MyMethod()
+            {
+                myFavouriteVariables++;
+                return true;
+            }
+        }
+
     }
-}
+    }
+
